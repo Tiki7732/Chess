c0655104e2ce31219213177b315ee27dd17e9318
tiki7732 <lsaidah@gmail.com>
2020-06-12 16:32:47 -0500

    added an empty method and changed a few things for testing

--- chess_board.rb
+++ chess_board.rb
@@ -1,12 +1,13 @@
 require_relative 'chess_piece'
+require_relative 'rook_piece'
 class Board
 
     def populate_board
         grid = Array.new(8) {Array.new(8)}
-        grid[0].each_index { |ind| grid[0][ind] = Piece.new("black", self, [0, ind]) }
-        grid[1].each_index { |ind| grid[1][ind] = Piece.new("black", self, [1, ind]) }
-        grid[6].each_index { |ind| grid[6][ind] = Piece.new("white", self, [6, ind]) }
-        grid[7].each_index { |ind| grid[7][ind] = Piece.new("white", self, [7, ind]) }
+        # grid[0].each_index { |ind| grid[0][ind] = Rook.new("black", self, [0, ind]) }
+        # grid[1].each_index { |ind| grid[1][ind] = Piece.new("black", self, [1, ind]) }
+        # grid[6].each_index { |ind| grid[6][ind] = Piece.new("white", self, [6, ind]) }
+        # grid[7].each_index { |ind| grid[7][ind] = Piece.new("white", self, [7, ind]) }
         grid
     end
 
@@ -42,6 +43,11 @@ class Board
         true
     end
 
+    def empty?(pos)
+        self[pos].nil?
+    end
+
+
     def show_board
         grid.each do |row|
             row.each {|space| print space.to_s + " "}
@@ -53,4 +59,5 @@ end
 
 b = Board.new
 b.show_board
-
+r = Rook.new("black", b, [0,0])
+p r.moves
