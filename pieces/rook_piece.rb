2ec60c4aa66e8c1fb933318c62f2a6ba82878e9f
tiki7732 <lsaidah@gmail.com>
2020-06-12 16:38:24 -0500

    moved into pieces folder

---
+++ pieces/rook_piece.rb
@@ -0,0 +1,18 @@
+require_relative 'chess_piece'
+require_relative 'slideable'
+class Rook < Piece
+    include Slideable
+
+    attr_reader :color, :board, :pos
+ 
+
+    def symbol 
+        '♜'.colorize(color)
+    end
+
+    protected
+    def move_dirs
+        horizontal_and_vertical
+    end
+end
+
