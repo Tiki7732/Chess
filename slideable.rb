85b0aec0c823a9dfe6de221ba6a93caca68c7254
tiki7732 <lsaidah@gmail.com>
2020-06-12 15:53:30 -0500

    made seperate slideable module

---
+++ slideable.rb
@@ -0,0 +1,17 @@
+module Slideable
+    HORIZONTAL_AND_VERTICAL = [[-1,0], [1, 0], [0, -1], [0, 1]
+    DIAGONAL = [[-1,-1], [-1,1], [1, -1], [1,1]]
+    
+    def horizontal_and_vertical
+        HORIZONTAL_AND_VERTICAL
+    end
+
+    def diagonal
+        DIAGONAL
+    end
+
+    def moves
+        moves = []
+    end
+
+end
