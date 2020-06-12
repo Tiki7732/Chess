790e406f23920429905618ca74b5bfc612aa9659
tiki7732 <lsaidah@gmail.com>
2020-06-12 15:12:58 -0500

    Changed initialize method and added a bunch of empty methods

--- chess_piece.rb
+++ chess_piece.rb
@@ -1,8 +1,32 @@
 class Piece
 
-    attr_reader :color
-    def initialize(color)
+    attr_reader :color, :board, :pos
+    def initialize(color, board, pos)
         @color = color
+        @board = board
+        @pos = pos
+    end
+
+    def to_s
+        color
+    end
+
+    def moves
+
+    end
+
+    def pos=(val)
+        pos = val
+    end
+
+    def symbol
+
+    end
+
+    private
+
+    def moves_into_check?(end_pos)
+
     end
 
 end
