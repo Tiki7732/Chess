8aaeaa1877197f6ac24f04ceda93aad3c832636d
tiki7732 <lsaidah@gmail.com>
2020-06-12 12:55:31 -0500

    added move_piece method and valid_pos? method

--- chess_board.rb
+++ chess_board.rb
@@ -19,12 +19,28 @@ class Board
 
     def [](pos)
         x, y = pos[0], pos[1]
-        @grid[x][y]
+        grid[x][y]
     end
 
     def []=(pos, val)
         x, y = pos[0], pos[1]
-        @grid[x][y] = val
+        grid[x][y] = val
+    end
+
+    def move_piece(start_pos, end_pos)
+        raise "No piece at this position" if self[start_pos].nil?
+        raise "Can't move there!" if !valid_pos?(end_pos)
+        piece = self[start_pos]
+        self[start_pos] = nil
+        self[end_pos] = piece
+
+    end
+
+    def valid_pos?(pos)
+        x, y = pos[0], pos[1]
+        return false if !x.between?(0, 7)
+        return false if !y.between?(0, 7)
+        true
     end
 
 end
