bc008204db0a6601464fc5a554eccc55b5fcfabf
tiki7732 <lsaidah@gmail.com>
2020-06-15 15:57:20 -0500

    filled out knight class

--- pieces/knight.rb
+++ pieces/knight.rb
@@ -3,6 +3,17 @@ require_relative 'stepable'
 require 'colorize'
 
 class Knight < Piece
-    include stepable
+    include Stepable
+
+    def symbol
+        '♞'.colorize(color)
+    end
+
+    protected
+
+    def moves_diff
+        moves = [[-2,-1], [-2,1], [2, -1], [2,1], [1,2], [1,-2], [-1,2], [-1,-2]]
+        moves
+    end
 
 end
