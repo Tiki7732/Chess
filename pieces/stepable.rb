7e3085ef588b25a39a2f00faa29801cedd43cb7b
tiki7732 <lsaidah@gmail.com>
2020-06-15 15:58:08 -0500

    fixed typos in code

--- pieces/stepable.rb
+++ pieces/stepable.rb
@@ -3,8 +3,8 @@ module Stepable
     def moves
         moves = []
         moves_diff.each do |new_pos|
-            move_x, move_y = new_pos[0], new_pos[y]
-            x, y = pos[0], pos[y]
+            move_x, move_y = new_pos[0], new_pos[-1]
+            x, y = pos[0], pos[-1]
             possible_move = [x + move_x, y + move_y]
             next unless board.valid_pos?(possible_move)
             moves.push(possible_move) if board.empty?(possible_move)
