0ea3eef8ed77e157f295eda4b60ca3e70a0106ca
tiki7732 <lsaidah@gmail.com>
2020-06-15 17:20:30 -0500

    refactored populate_board method

--- chess_board.rb
+++ chess_board.rb
@@ -2,20 +2,12 @@ require_relative 'pieces'
 require 'colorize'
 class Board
 
-    def populate_board
-        grid = Array.new(8) {Array.new(8)}
-        # grid[0].each_index { |ind| grid[0][ind] = Rook.new("black", self, [0, ind]) }
-        # grid[1].each_index { |ind| grid[1][ind] = Piece.new("black", self, [1, ind]) }
-        # grid[6].each_index { |ind| grid[6][ind] = Piece.new("white", self, [6, ind]) }
-        # grid[7].each_index { |ind| grid[7][ind] = Piece.new("white", self, [7, ind]) }
-        grid
-    end
-
-    public
-    attr_reader :grid
+    attr_reader :grid, :sentinel
 
     def initialize
-        @grid = self.populate_board
+        @sentinel = NullPiece.instance
+        @grid = Array.new(8) {Array.new(8, sentinel)}
+        self.populate_board
     end
 
     def [](pos)
@@ -44,9 +36,31 @@ class Board
     end
 
     def empty?(pos)
-        self[pos].nil?
+        self[pos].empty?
     end
 
+    def populate_board
+       
+        [:red, :green].each do |color|
+            fill_back_row(color)
+            fill_pawn_row(color)
+        end
+    end
+
+    def fill_back_row(color)
+        start_row = color == :red ? 7 : 0
+        back_row = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+        grid[start_row].map!.with_index do |piece, ind|
+            piece = back_row[ind].new(color, self, [start_row, ind])
+        end
+    end
+
+    def fill_pawn_row(color)
+        start_row = color == :red ? 6 : 1
+        8.times do |num| 
+            grid[start_row][num] = Pawn.new(color, self, [start_row, num])
+        end
+    end
 
     def show_board
         grid.each do |row|
@@ -63,23 +77,25 @@ class Board
 end
 
 b = Board.new
-r = Rook.new(:green, b, [0,0])
-b.add_piece(r, r.pos)
-
-r2 = Rook.new(:blue, b, [7,0])
-b.add_piece(r2, r2.pos)
-q = Queen.new(:green, b, [0,3])
-b.add_piece(q, q.pos)
-
-bishop = Bishop.new(:green, b, [0,2])
-k = Knight.new(:green, b, [0,1])
-king = King.new(:green, b, [0,4])
-b.add_piece(king, king.pos)
-b.add_piece(k, k.pos)
-b.add_piece(bishop, bishop.pos)
 b.show_board
-# p r.moves
-# p bishop.moves
-# p r2.moves
-# p q.moves
-p king.moves
+#p b.empty?([2,0])
+# r = Rook.new(:green, b, [0,0])
+# b.add_piece(r, r.pos)
+
+# r2 = Rook.new(:blue, b, [7,0])
+# b.add_piece(r2, r2.pos)
+# q = Queen.new(:green, b, [0,3])
+# b.add_piece(q, q.pos)
+
+# bishop = Bishop.new(:green, b, [0,2])
+# k = Knight.new(:green, b, [0,1])
+# king = King.new(:green, b, [0,4])
+# b.add_piece(king, king.pos)
+# b.add_piece(k, k.pos)
+# b.add_piece(bishop, bishop.pos)
+# b.show_board
+# # p r.moves
+# # p bishop.moves
+# # p r2.moves
+# # p q.moves
+# p king.moves
