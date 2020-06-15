c867518eeb470988f22579fc0b239d7396cdaeda
tiki7732 <lsaidah@gmail.com>
2020-06-15 16:23:47 -0500

    filling out inititiale and empty? methods

--- pieces/nullpiece.rb
+++ pieces/nullpiece.rb
@@ -1,3 +1,19 @@
-class NullPiece < Piece
+require_relative 'piece'
+require 'singleton'
 
+class NullPiece < Piece
+    include Singleton
+    attr_reader :symbol
+    def initialize
+        @symbol = " "
+        @color = :none
+    end
+
+    def moves
+        []
+    end
+
+    def empty?
+        true
+    end
 end
