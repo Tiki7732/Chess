f40ae7a4dad84c45f088e67f612938daabd801bb
tiki7732 <lsaidah@gmail.com>
2020-06-15 16:07:29 -0500

    filled out king subclass

--- pieces/king.rb
+++ pieces/king.rb
@@ -1,3 +1,18 @@
+require_relative 'piece'
+require_relative 'stepable'
+require 'colorize'
+
 class King < Piece
 
+    include Stepable
+
+    def symbol
+        '♚'.colorize(color)
+    end
+
+    protected
+    def move_diffs
+        moves = [[-1,-1], [-1,1], [-1,0], [1, -1], [1,1], [1, 0], [0, -1], [0, 1]
+        moves
+    end
 end
