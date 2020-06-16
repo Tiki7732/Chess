7063ad08c831689118be0f297d6b8989081607db
tiki7732 <lsaidah@gmail.com>
2020-06-16 13:20:46 -0500

    fixed moves method

--- pieces/stepable.rb
+++ pieces/stepable.rb
@@ -2,13 +2,14 @@ module Stepable
 
     def moves
         moves = []
+        
         moves_diff.each do |new_pos|
             move_x, move_y = new_pos[0], new_pos[-1]
             x, y = pos[0], pos[-1]
             possible_move = [x + move_x, y + move_y]
             next unless board.valid_pos?(possible_move)
             moves.push(possible_move) if board.empty?(possible_move)
-            moves.push(possible_move) if board[pos].color != color
+            moves.push(possible_move) if board[possible_move].color != color
         end
         moves
     end
