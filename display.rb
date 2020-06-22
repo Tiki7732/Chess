78933f3b7722f29779c25e334096a0ff3729c754
tiki7732 <lsaidah@gmail.com>
2020-06-22 13:23:27 -0400

    filling initialize

--- display.rb
+++ display.rb
@@ -2,3 +2,19 @@ require 'colorize'
 require_relative 'chess_board'
 require_relative 'cursor'
 
+class Display
+
+    
+    def initialize(board)
+        @board = board
+        @cursor = Cursor.new([0,0], board)
+    end
+
+    def show_board
+        @board.show_board
+    end
+end
+
+board = Board.new
+d = Display.new(board)
+d.show_board
