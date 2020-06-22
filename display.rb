8bdaf15a9d4accda3a3de9e2b64c0606539da70e
tiki7732 <lsaidah@gmail.com>
2020-06-22 13:52:43 -0400

    testing methods

--- display.rb
+++ display.rb
@@ -4,7 +4,8 @@ require_relative 'cursor'
 
 class Display
 
-    
+    attr_accessor :cursor
+
     def initialize(board)
         @board = board
         @cursor = Cursor.new([0,0], board)
@@ -12,9 +13,13 @@ class Display
 
     def show_board
         @board.show_board
+        @cursor.get_input
     end
 end
 
 board = Board.new
 d = Display.new(board)
 d.show_board
+# p d.cursor.cursor_pos
+# d.cursor.update_pos([2,2])
+# p d.cursor.cursor_pos
