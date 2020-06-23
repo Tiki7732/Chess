36f312b3e92c66306758a388648694691aeac9e7
tiki7732 <lsaidah@gmail.com>
2020-06-23 14:34:41 -0400

    added a switch players method

--- chess_game.rb
+++ chess_game.rb
@@ -1,3 +1,18 @@
+require_relative 'chess_board'
+require_relative 'display'
+require_relative 'chess_player'
+
 class Game
 
+    def initialize
+        @board = Board.new
+        @display = Display.new(board)
+        @player1 = Player.new
+        @player2 = Player.new
+        @current_player = :white
+    end
+
+    def switch_players(color)
+        color == :black ? :white : :black
+    end
 end
