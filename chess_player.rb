5f5d9ec989eaa5e3156516d6343f309aed37bd5d
tiki7732 <lsaidah@gmail.com>
2020-06-24 13:27:53 -0400

    filled out make_move method

--- chess_player.rb
+++ chess_player.rb
@@ -1,22 +1,30 @@
 require_relative 'display'
 class Player
 
+    attr_reader :color, :display
     def initialize(color, display)
         @color = color
         @display = display
     end
 
-    def make_move
-        input = @display.get_input
-        case input
-        when 'quit'
-            return 'quit'
-        when input.is_a?(Array)
-            return input
-        when true
-            return true
-        else
-            return nil
+    def make_move(board)
+        start_pos = nil
+        end_pos = nil
+        
+        until start_pos && end_pos
+            display.render
+            if start_pos
+                p "Move to where?"
+                end_pos = display.cursor.get_input
+
+            else
+                p "It's #{color}'s turn to move!"
+                start_pos = display.cursor.get_input
+
+            end
         end
+
+        [start_pos, end_pos]
+
     end
 end
