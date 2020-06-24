692ad349f040970aadb58b3f36ab76fdbe171718
tiki7732 <lsaidah@gmail.com>
2020-06-24 12:00:45 -0400

    filled out make_move method

--- chess_player.rb
+++ chess_player.rb
@@ -1,3 +1,4 @@
+require_relative 'display'
 class Player
 
     def initialize(color, display)
@@ -6,6 +7,16 @@ class Player
     end
 
     def make_move
-        input = 
+        input = @display.get_input
+        case input
+        when 'quit'
+            return 'quit'
+        when input.is_a?(Array)
+            return input
+        when true
+            return true
+        else
+            return nil
+        end
     end
 end
